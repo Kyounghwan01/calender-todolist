31e02611db0524394b90236f397c7bcd7c0012fa
kyounghwan Noh <44187477+Kyounghwan01@users.noreply.github.com>
2019-05-15 10:58:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -95,3 +95,51 @@ function buildCalendar() {
     event.target.classList.add("pointDay");
   }
 }
+
+// ----------------------------------------- todo list 
+// --------------------------------------------------- 
+// ---------------------------------------------------
+// ----------------------------------------- todo list 
+
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+function paintToDo(text) {
+    // 리스트 넣을 li 엘리먼트 생성 
+  const li = document.createElement("li");
+  //삭제버튼 생성
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "❌";
+  const span = document.createElement("span");
+  span.innerText = text;
+  li.appendChild(delBtn);
+  //li 안에 버튼 넣기
+  li.appendChild(span);
+  //li안에 span 넣기
+  toDoList.appendChild(li);
+  //toDoList안에 li 넣기 
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  //쓰고 엔터시 빈공간
+  toDoInput.value = "";
+}
+
+function loadToDos() {
+  const toDos = localStorage.getItem(TODOS_LS);
+  if (toDos !== null) {
+  }
+}
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
