c84cf3053ec04aa2bc176af2407d75a74bd281df
kyounghwan Noh <44187477+Kyounghwan01@users.noreply.github.com>
2019-05-31 19:16:24 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,70 @@
-
-let today = new Date(); //sat May 25 2019
-let currentMonth = today.getMonth(); //4
-let currentYear = today.getFullYear(); //2019
-let monthAndYear = document.getElementById("monthAndYear");
-let todayKeyValue = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
+let today = new Date(),
+  temp = new Date(),
+  currentMonth = today.getMonth(),
+  currentYear = today.getFullYear(),
+  monthAndYear = document.getElementById("monthAndYear"),
+  pointId,
+  tempKeyId,
+  textData = [],
+  monthText = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ],
+  weekText = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ],
+  todayKeyValue = today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear();
 const toDoForm = document.querySelector(".js-toDoForm"),
   toDoInput = toDoForm.querySelector("input"),
   toDoList = document.querySelector(".js-toDoList");
-
-let monthText = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-];
-let weekText = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
+window.prevCalendar = prevCalendar;
+window.nextCalendar = nextCalendar;
 
 function prevCalendar() {
   currentYear = currentMonth === 0 ? currentYear - 1 : currentYear;
   currentMonth = currentMonth === 0 ? 11 : currentMonth - 1;
-  today = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
-  showCalendar(currentYear, currentMonth);
+  if (currentMonth === 4 && currentYear === 2019) {
+    today = new Date(today.getFullYear(), today.getMonth() - 1, temp.getDate());
+    showCalendar(currentYear, currentMonth);
+    loadToDos(temp.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear());
+  } else {
+    today = new Date(today.getFullYear(), today.getMonth() - 1);
+    todayKeyValue = today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear()
+    showCalendar(currentYear, currentMonth);
+    loadToDos(today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear());
+  }
 }
 
 function nextCalendar() {
   currentYear = currentMonth === 11 ? currentYear + 1 : currentYear;
   currentMonth = currentMonth === 11 ? 0 : currentMonth + 1;
-  today = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate());
-  showCalendar(currentYear, currentMonth);
+  if (currentMonth === 4 && currentYear === 2019) {
+    today = new Date(today.getFullYear(), today.getMonth() + 1, temp.getDate());
+    showCalendar(currentYear, currentMonth);
+    loadToDos(temp.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear());
+  } else {
+    today = new Date(today.getFullYear(), today.getMonth() + 1);
+    todayKeyValue = today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear();
+    showCalendar(currentYear, currentMonth);
+    loadToDos(today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear());
+    point(todayKeyValue);
+  }
 }
 
 function showCalendar(year, month) {
@@ -96,8 +117,6 @@ function showCalendar(year, month) {
   }
 }
 
-let pointId;
-
 function point(cell) {
   let Point = event.target.firstChild.nodeValue;
   pointDate.innerHTML = Point;
@@ -112,37 +131,25 @@ function point(cell) {
   pointId = document.getElementById(cell.id).id;
   loadToDos(cell.id);
   write(cell.id);
-  
 }
 
-// -------------------------------------------- todo list
-// ------------------------------------------------------
-// -------------------------------------------- todo list
-
-let textData = [];
-
-if(JSON.parse(localStorage.getItem(todayKeyValue)) === null){
+if (JSON.parse(localStorage.getItem(todayKeyValue)) === null) {
   textData = [];
-  console.log(JSON.parse(localStorage.getItem(todayKeyValue)));
-}else{
-  textData = (JSON.parse(localStorage.getItem(todayKeyValue)));
+} else {
+  textData = JSON.parse(localStorage.getItem(todayKeyValue));
 }
 
-
-let tempKeyId;
 function write(cell, currentValue) {
   if (currentValue === undefined || currentValue === "") return;
   const keyId = document.getElementById(cell).id;
-  if(tempKeyId === undefined){
+  if (tempKeyId === undefined) {
     tempKeyId = keyId;
   }
-  if(tempKeyId !== keyId){
-    if(JSON.parse(localStorage.getItem(keyId)) === null){
+  if (tempKeyId !== keyId) {
+    if (JSON.parse(localStorage.getItem(keyId)) === null) {
       textData = [];
-      console.log(JSON.parse(localStorage.getItem(keyId)));
-    }else{
-      textData = (JSON.parse(localStorage.getItem(keyId)));
-      //textData.push(JSON.parse(localStorage.getItem(keyId)));
+    } else {
+      textData = JSON.parse(localStorage.getItem(keyId));
     }
   }
   const textObj = { text: currentValue };
@@ -155,10 +162,9 @@ function handleSubmit(event) {
   event.preventDefault();
   const currentValue = toDoInput.value;
   paintToDo(currentValue);
-  if(pointId === undefined){
+  if (pointId === undefined) {
     pointId = todayKeyValue;
   }
-  //이거 문제얌!, cell에 들어가는 key 값
   write(pointId, currentValue);
   toDoInput.value = "";
 }
@@ -166,15 +172,10 @@ function handleSubmit(event) {
 function loadToDos(cell) {
   const loadId = document.getElementById(cell).id;
   const loadedToDos = JSON.parse(localStorage.getItem(loadId));
-  if(loadedToDos){
-    sampleData = loadedToDos;
-  }
-  //기존 li 다 삭제
   let lis = document.querySelectorAll("li");
   for (let i = 0; (li = lis[i]); i++) {
     li.parentNode.removeChild(li);
   }
-  //li에 추가할 text 출력
   if (loadedToDos) {
     loadedToDos.forEach(function(o) {
       paintToDo(o.text);
@@ -186,36 +187,12 @@ function paintToDo(text) {
   if (text !== "") {
     const li = document.createElement("li");
     li.style.overflow = "hidden";
-    //삭제버튼 생성
-    // const delBtn = document.createElement("i");
-    // delBtn.style.cursor = "pointer";
-    // delBtn.addEventListener("click", deleteToDo);
     const span = document.createElement("span");
     span.style.float = "left";
     span.style.fontSize = "15px";
     span.style.letterSpacing = "0.5px";
-    //const newId = toDos.length + 1;
-    // delBtn.className = "fas fa-backspace";
-    // delBtn.style.float = "right";
     span.innerText = "- " + text;
-
-    //li.appendChild(delBtn);
     li.appendChild(span);
-    //li.id = newId;
-
-    //console.log(a);
-    //만약 동일한 id가 html에 있으면 리스트에 appendchild하지않음
-    //console.log(toDoList.getElementsByTagName('li'));
-
-    // const listItems = toDoList.getElementsByTagName("li");
-
-    // if (listItems.length) {
-    //   if (listItems[0].id !== li.id) {
-    //     toDoList.appendChild(li);
-    //   }
-    // } else {
-    //   toDoList.appendChild(li);
-    // }
     toDoList.appendChild(li);
   }
 }
